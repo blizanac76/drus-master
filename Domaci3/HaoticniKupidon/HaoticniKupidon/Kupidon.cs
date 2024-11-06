1b2d83efdcb92b834a30a1f5d68dcd25804ffa76
mladen <mladenblizanac@gmail.com>
2024-11-06 20:23:47 +0100

    Pocetak domaci3

---
+++ Domaci3/HaoticniKupidon/HaoticniKupidon/Kupidon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Threading;
+using System.Web;
+using System.Timers;
+
+namespace HaoticniKupidon
+{
+    
+    public class Kupidon
+    {
+        public System.Timers.Timer _timer;
+        
+        private void SetTimer()
+        {
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += nesto;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        
+    }
+}
---
+++ Domaci3/HaoticniKupidon/Klijent/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Emit;
+
+Console.WriteLine("Unesite ime");
+labelaIme:
+string? ime = Console.ReadLine();
+if (ime == null)
+{
+    Console.WriteLine("unestie Ime u pritisni ENTER!");
+    goto labelaIme;
+}
+
+Console.WriteLine("Unesite Grad");
+labelaGrad:
+string? grad =Console.ReadLine();
+
+Console.WriteLine("Unesite Vase godine");
+labelaGodine:
+if (!Int32.TryParse(Console.ReadLine(), out int godine))
+{
+    Console.WriteLine("unestie Godine u ispravnom celobrojnom formatu i pritisni ENTER!");
+    goto labelaGodine;
+}
+
+Console.WriteLine("Unesite Broj telefona");
+labelaBrojTel:
+string? brTel = Console.ReadLine();
+if (brTel == null)
+{
+    Console.WriteLine("unestie broj Telefona i pritisni ENTER!");
+    goto labelaBrojTel;
+}
+
+Console.WriteLine($"Ime: {ime} Grad: {grad} Godine: {godine} Broj Telefona {brTel}");
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
